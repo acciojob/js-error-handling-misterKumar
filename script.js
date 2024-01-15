d322a3a86ae9ac676d5e97a80921bbb923947f4f
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-15 14:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 class OutOfRangeError extends Error {
-	constructor(params) {
-		super(params);
-		this.name = "OutOfRangeError";
-	}
+  constructor(arg) {
+    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
+    this.name = this.constructor.name;
+  }
 }
 
-class InvalidExprError extends Error{
-	constructor(params) {
-		super(params);
-		this.name = "InvalidExprError";
-	} 
-} 
+class InvalidExprError extends Error {
+  constructor() {
+    super('Expression should not have an invalid combination of expression');
+    this.name = this.constructor.name;
+  }
+}
 
-let btn = document.getElementById("btn");
+function evalString(expr) {
+  if (/[\+\-\*\/]{2,}/.test(expr)) {
+    throw new InvalidExprError();
+  }
 
-btn.addEventListener('click', (e)=>{
-	let input = document.getElementById("input").value;
-	let exprarray = input.split("");
+  if (/^[\+\*\/]/.test(expr)) {
+    throw new SyntaxError('Expression should not start with invalid operator');
+  }
+
+  if (/[\+\*\/\-]$/.test(expr)) {
+    throw new SyntaxError('Expression should not end with invalid operator');
+  }
+
+  if (/[^0-9+\-*/\s]/.test(expr)) {
+    throw new OutOfRangeError(expr.match(/[^0-9+\-*/\s]/)[0]);
+  }
+
+  return eval(expr);
+}
+
 try {
-	if(!(exprarray[0] >= '0' && exprarray[0] <= '9'))
-		throw new OutOfRangeError("Expression should not start with invalid operator");
-	document.write(eval(input));
-	
-} catch (OutOfRangeError) {
-	document.write(OutOfRangeError.message);
+  const result = evalString('1 + 2 * 3');
+  console.log(result); // 7
+} catch (e) {
+  console.error(e);
 }
-
-});
