8245788cc25db2173b42f04c5a062f6d469351dd
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-15 09:21:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-//your code here
-
 class OutOfRangeError extends Error {
-  constructor(arg) {
-    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
-    this.name = this.constructor.name;
-  }
+	constructor(params) {
+		super(params);
+		this.name = "OutOfRangeError";
+	}
 }
 
-class InvalidExprError extends Error {
-  constructor() {
-    super('Expression should not have an invalid combination of expression');
-    this.name = this.constructor.name;
-  }
-}
+class InvalidExprError extends Error{
+	constructor(params) {
+		super(params);
+		this.name = "InvalidExprError";
+	} 
+} 
 
-function evalString(expr) {
-  // Check for invalid operators combinations
-  if (expr.includes('++') || expr.includes('--') || expr.includes('//') || expr.includes('/*') || expr.includes('*/') || expr.includes('+/') || expr.includes('-/')) {
-    throw new InvalidExprError();
-  }
+let btn = document.getElementById("btn");
 
-  // Check for invalid start and end operators
-  if (/^[+/*]/.test(expr)) {
-    throw new SyntaxError('Expression should not start with invalid operator');
-  } else if (/[-+/*]$/.test(expr)) {
-    throw new SyntaxError('Expression should not end with invalid operator');
-  }
-
-  // Check for invalid characters
-  if (!/^[0-9+\-*/\s]+$/.test(expr)) {
-    throw new OutOfRangeError(expr.match(/[^0-9+\-*/\s]/)[0]);
-  }
-
-  // Evaluate the expression
-  return eval(expr);
-}
-
-// Test the function with some valid and invalid expressions
+btn.addEventListener('click', (e)=>{
+	let input = document.getElementById("input").value;
+	let exprarray = input.split("");
 try {
-  console.log(evalString('1+2-3*4/5')); // Output: -0.6
-  console.log(evalString('-10 + 5')); // Output: -5
-  console.log(evalString('1 + a')); // Throws OutOfRangeError
-  console.log(evalString('1++2')); // Throws InvalidExprError
-  console.log(evalString('*2+3')); // Throws SyntaxError
-  console.log(evalString('4/2-')); // Throws SyntaxError
-} catch (error) {
-  console.error(error.name + ': ' + error.message);
+	if(!(exprarray[0] >= '0' && exprarray[0] <= '9'))
+		throw new OutOfRangeError("Expression should not start with invalid operator");
+	document.write(eval(input));
+	
+} catch (OutOfRangeError) {
+	document.write(OutOfRangeError.message);
 }
+
+});
